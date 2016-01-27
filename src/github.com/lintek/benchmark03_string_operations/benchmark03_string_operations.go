0660102f15c0eb0ca5903d1023ee7955f6fc5780
zborowski <zborowski@antmicro.com>
2016-01-27 14:41:38 +0100

    Fixed.

--- src/github.com/lintek/benchmark03_string_operations/benchmark03_string_operations.go
+++ src/github.com/lintek/benchmark03_string_operations/benchmark03_string_operations.go
@@ -1,88 +1,104 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"time"
-	"runtime"
-	"strings"
-	"bytes"
+    "fmt"
+    "os"
+    "time"
+    "runtime"
+    "strings"
+    "bytes"
 )
-
 
 const (
-	NUM_ITERATIONS = 200000
-	NUM_TASKS = 50
+    NUM_ITERATIONS = 200000
+    NUM_TASKS = 50
 )
 
-
 func prepareData() [] string {
-	data := make([]string, 0)
+    data := make([]string, 0)
 
-    for c := 0x30; c < 0x79; c+=2 {
-		item := fmt.Sprintf("%c%c%c", c, c+1, c+2)
-		data = append(data, item)
-	}
+    for c := 0x30; c < 0x79; c += 3 {
+        item := fmt.Sprintf("%c%c%c", c, c + 1, c + 2)
+        data = append(data, item)
+    }
 
-	return data
+    return data
 }
 
-func function1(inputData []string, c chan string) {
-	result := ""
+func function1(inputData []string, c chan []string) {
+    result := make([]string, NUM_ITERATIONS)
 
-	for i:= 0; i < len(inputData); i++ {
-		result += inputData[i]
-	}
+    for i := 0; i < NUM_ITERATIONS; i++ {
+        output_item := ""
+
+        for j := 0; j < len(inputData); j++ {
+            output_item += inputData[j]
+        }
+
+        result[i] = output_item
+    }
 
     c <- result
-	close(c)
+    close(c)
 }
 
-func function2(inputData []string, c chan string) {
-	c <- strings.Join(inputData, "")
-	close(c)
+func function2(inputData []string, c chan []string) {
+    result := make([]string, NUM_ITERATIONS)
+
+    for i := 0; i < NUM_ITERATIONS; i++ {
+        result[i] = strings.Join(inputData, "")
+    }
+
+    c <- result
+    close(c)
 }
 
-func function3(inputData []string, c chan string) {
-	var result bytes.Buffer
+func function3(inputData []string, c chan []string) {
+    result := make([]string, NUM_ITERATIONS)
 
-	for i:= 0; i < len(inputData); i++ {
-		result.WriteString(inputData[i])
-	}
+    for i := 0; i < NUM_ITERATIONS; i++ {
+        var output_item bytes.Buffer
 
-	c <- result.String()
-	close(c)
+        for j := 0; j < len(inputData); j++ {
+            output_item.WriteString(inputData[j])
+        }
+
+        result[i] = output_item.String()
+    }
+
+    c <- result
+    close(c)
 }
 
 func main() {
-	fmt.Printf("Running: %s\n", os.Args[0])
+    fmt.Printf("Running: %s\n", os.Args[0])
 
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	fmt.Printf("Number of available CPUs: %d\n", runtime.NumCPU())
+    runtime.GOMAXPROCS(runtime.NumCPU())
+    fmt.Printf("Number of available CPUs: %d\n", runtime.NumCPU())
 
-	myData := prepareData()
-	/*fmt.Printf("myData: %v\n", myData)*/
+    myData := prepareData()
+    /*fmt.Printf("myData: %v\n", myData)*/
 
-	funcs := []func([]string, chan string) {function1, function2, function3}
-	for f := range funcs {
+    funcs := []func([]string, chan []string){function1, function2, function3}
+    for f := range funcs {
 
-		var chans [NUM_TASKS] chan string
-		for i := range chans {
-		   chans[i] = make(chan string)
-		}
+        var chans [NUM_TASKS] chan []string
+        for i := range chans {
+            chans[i] = make(chan []string)
+        }
 
-		startTime := time.Now()
+        startTime := time.Now()
 
-		for i := range chans {
-			go funcs[f](myData, chans[i])
-		}
+        for i := range chans {
+            go funcs[f](myData, chans[i])
+        }
 
-		stopTime := time.Now()
+        stopTime := time.Now()
 
-		fmt.Printf("Execution time for function%d: %f\n", f+1, stopTime.Sub(startTime).Seconds())
+        fmt.Printf("Execution time for function%d: %f\n", f + 1, stopTime.Sub(startTime).Seconds())
 
-		/*for i := range chans {
-		   fmt.Printf("Generated output for task %d: %s\n", i, <-chans[i])
-		}*/
-	}
+        /*for i := range chans {
+            fmt.Printf("Generated output for task %d: %s\n", i, <-chans[i])
+        }*/
+    }
 }
